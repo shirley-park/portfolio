fae39d18b507413f1501a64facdf5b04f13977d6
Shirley Park <shirley.park7@gmail.com>
2023-04-26 23:17:20 +1200

    add assets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
-      {/* <Head>
+      <Head>
         <title>Shirley Park | Fullstack developer</title>
-
-      </Head> */}
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Navbar />
       <main></main>
     </div>
   )
