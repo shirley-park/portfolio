8b4132826daa7c0a61c95466b72c548658846391
Shirley Park <shirley.park7@gmail.com>
2023-04-26 23:18:03 +1200

    make navbar component and style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <div className="fixed w-full h-20 z=[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src="/../public/assets/homeicon.png"
+          alt="/"
+          width="40"
+          height="50"
+        />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm hover:bg-[#e0e0e0]">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm hover:bg-[#e0e0e0]">Projects</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm hover:bg-[#e0e0e0]">Contact</li>
+            </Link>
+          </ul>
+          {/* <div></div> */}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
