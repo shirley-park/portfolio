4a6d85e58bfef9545f70095474c585fa80a2f25a
Shirley Park <shirley.park7@gmail.com>
2023-04-29 12:39:13 +1200

    open and close navbar overlay

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 import { FaLinkedinIn, FaGithub, FaRegEnvelope } from 'react-icons/fa'
 
 function Navbar() {
+  const [nav, setNav] = useState(false)
+
+  function handleNav() {
+    setNav(!nav)
+  }
+
   return (
     <div className="fixed w-full h-20 z=[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
@@ -17,23 +23,32 @@ function Navbar() {
         <div>
           <ul className="hidden md:flex">
             <Link href="/">
-              <li className="ml-10 text-sm hover:bg-[#e0e0e0]">About</li>
+              <li className="ml-10 text-sm hover:border-b">About</li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm hover:bg-[#e0e0e0]">Projects</li>
+              <li className="ml-10 text-sm hover:border-b">Projects</li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm hover:bg-[#e0e0e0]">Contact</li>
+              <li className="ml-10 text-sm hover:border-b">Contact</li>
             </Link>
           </ul>
-          <div className="md:hidden">
+          <div onClick={handleNav} className="md:hidden cursor-pointer">
             <AiOutlineMenu size={25} />
           </div>
         </div>
       </div>
 
-      <div className="fixed left-0 top-0 w-full h-screen bg-black/50">
-        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+      {/* Mobile left-side nav */}
+      <div
+        className={nav ? 'fixed left-0 top-0 w-full h-screen bg-black/50' : ''}
+      >
+        <div
+          className={
+            nav
+              ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+              : 'fixed left-[-100%] top-0 w-[75%] p-10 ease-in duration-500'
+          }
+        >
           <div className="flex w-full items-center justify-between">
             <Image
               src="/../public/assets/homeicon.png"
@@ -41,7 +56,10 @@ function Navbar() {
               width="40"
               height="50"
             />
-            <div className="border-2 rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer">
+            <div
+              onClick={handleNav}
+              className="border-2 rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer"
+            >
               <AiOutlineClose />
             </div>
           </div>
@@ -64,13 +82,13 @@ function Navbar() {
                 Let&apos;s Connect
               </p>
               <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
-                <div className="border-2 rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer">
+                <div className="border-2 rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-200">
                   <FaLinkedinIn />
                 </div>
-                <div className="border-2 rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer">
+                <div className="border-2 rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-200">
                   <FaGithub />
                 </div>
-                <div className="border-2 rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer">
+                <div className="border-2 rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-200">
                   <FaRegEnvelope />
                 </div>
               </div>
