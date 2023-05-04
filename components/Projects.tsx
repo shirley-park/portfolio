2e8c7cdf37c478579c0a7b8dcfb2080cae256b5e
Shirley Park <shirley.park7@gmail.com>
2023-05-04 15:07:26 +1200

    add pt to projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,7 +5,7 @@ import nookProjectImage from '../public/assets/projects/nookProjectImage2.png'
 
 function Projects() {
   return (
-    <div id="projects" className="w-full h-full pt-20">
+    <div id="projects" className="w-full h-full pt-24">
       <div className="max-w-[1240px] mx-auto w-[80%] md:w-[60%] h-full px-2">
         <h2 className="py-2 mb-8 border-b-2">Projects</h2>
         <div className="p-12 lg:mb-20 bg-[#f5f5f5] rounded-md">
