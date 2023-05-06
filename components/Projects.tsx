bb8c960864375384233bd3049ce964d76877afae
Shirley Park <shirley.park7@gmail.com>
2023-05-06 16:13:18 +1200

    change hover effect on projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,7 +8,7 @@ function Projects() {
     <div id="projects" className="w-full h-full pt-24">
       <div className="max-w-[1240px] mx-auto w-[80%] md:w-[60%] h-full px-2">
         <h2 className="py-2 mb-8 border-b-2">Projects</h2>
-        <div className="p-12 lg:mb-20 bg-[#f5f5f5] rounded-md">
+        <div className="p-14 lg:mb-20 bg-[#f5f5f5] rounded-xl">
           <ProjectItem
             title="Nook"
             projectImage={nookProjectImage}
