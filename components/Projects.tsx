8c64443683b8749e44abcbf7a9a48942ad3f053e
Shirley Park <shirley.park7@gmail.com>
2023-05-07 20:37:44 +1200

    fix scroll position restoration

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ProjectItem from './ProjectItem'
+import { useRouter } from 'next/router'
 
 import nookProjectImage from '../public/assets/projects/nookProjectImage2.png'
 
 function Projects() {
+  const router = useRouter()
+
+  // set scroll restoration to manual
+  useEffect(() => {
+    if (
+      'scrollRestoration' in history &&
+      history.scrollRestoration !== 'manual'
+    ) {
+      history.scrollRestoration = 'manual'
+    }
+  }, [])
+
+  // handle and store scroll position
+  useEffect(() => {
+    const handleRouteChange = () => {
+      sessionStorage.setItem('scrollPosition', window.scrollY.toString())
+    }
+    router.events.on('routeChangeStart', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+  }, [router.events])
+
+  // restore scroll position
+  useEffect(() => {
+    if ('scrollPosition' in sessionStorage) {
+      window.scrollTo(0, Number(sessionStorage.getItem('scrollPosition')))
+      sessionStorage.removeItem('scrollPosition')
+    }
+  }, [])
+
   return (
     <div id="projects" className="w-full h-full pt-24">
       <div className="max-w-[1240px] mx-auto w-[80%] md:w-[60%] h-full px-2">
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
 
 const inter = Inter({
   subsets: ['latin'],
